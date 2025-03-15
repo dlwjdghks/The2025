27339135475869294f524271d6dcd37f3c4f6d1e
TJ <TJ@DESKTOP-8FSFFJK>
2025-03-15 16:57:44 +0900

    FirstProj

---
+++ FirstProj/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int value = -1;
+		int value2 = 100;
+
+
+		System.out.println(value);
+        System.out.println(value2);
+
+	}
+
+}
---
+++ FirstProj/src/VariableExample.java
@@ -0,0 +1,11 @@
+
+public class VariableExample {
+
+	public static void main(String[] args) {
+		int value = -100;
+		
+		System.out.println(value);
+
+	}
+
+}
