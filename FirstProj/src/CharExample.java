0d323a1b308bb3f626418d9b9091c8b1f1cfb3d4
TJ <TJ@DESKTOP-8FSFFJK>
2025-03-15 17:31:06 +0900

    add

--- FirstProj/src/CharExample.java
+++ FirstProj/src/CharExample.java
@@ -1,13 +1,14 @@
-
 public class CharExample {
-
 	public static void main(String[] args) {
-		char c1 = 'A';
-		char c2 = 65;
+		int a = 5;
+		int b = 0;
 		
-		System.out.println(c1);
-		System.out.println(c2);
-
+		b += a;	// b = b + a;
+		b *= a; // b = b * a;
+		
+		System.out.println(a);
+		System.out.println(b);
+	
 	}
 
 }
---
+++ Jungol/src/q520/Main.java
@@ -0,0 +1,37 @@
+package q520;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num1, num2;
+		num1 = 55;
+		num2 = 10;
+		
+		System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+		
+		num1 = 2008;
+		num2 = 1999;
+		
+		System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));		
+/*		
+		int A = 55;
+		int B = 10;
+		int min = 0;
+		
+		min = A - B;
+		
+		int C = 2008;
+		int D = 1999;
+		int min2 = 9;
+		
+		int F = 9;
+		
+		System.out.println(A + " - " + B " = " + min);
+*/	
+		
+
+	}
+
+}
