6c83467e9a25c1fb626ff6e8e1991a62a27aefad
TJ <TJ@DESKTOP-8FSFFJK>
2025-03-15 16:41:29 +0900

    Initial commit

---
+++ Baekjoon/src/q1000/Main.java
@@ -0,0 +1,23 @@
+package q1000;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		sc.close();
+		
+// #1.
+		System.out.println(A + B);
+		
+// #2. 
+//		int sum = A + B;
+//		System.out.println(sum);
+		
+	}
+
+}
